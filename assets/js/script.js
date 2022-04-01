bff0711ef57318390a92894dbec9d204fc70adaa
Dan Høegh <dan.hoegh@gmail.com>
2022-04-01 11:26:52 +0200

    added typewriter stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,35 @@
-// JS by Dan Høegh
-// UCN MMD 2021
+// // JS by Dan Høegh
+// // UCN MMD 2021
+
+// // .substring()
+// // .indexOf()
+// // .split
+
+// const nameStr = "Sophie Ellis Baxter";
+// const nameSplit = nameStr.split(" ");
+// let initials = "";
+// nameSplit.forEach((item) =>{
+//     initials += item.substring(0,1);
+// });
+// document.querySelector("#app")
+// .innerHTML = `${nameStr} (${initials})`;
+    
+
+const typeStuff = () => {
+    const typeDiv = document.querySelector("#typewriter");
+    let currentText = "";
+    const text = typeDiv.innerHTML;
+
+    let i = 0;
+    let timer = setInterval(() => {
+        currentText = text.substring(0, i + 1);
+        typeDiv.innerHTML = currentText;
+        i++;
+        if (i == text.length) {
+            clearInterval(timer);
+            setTimeout(typeStuff, 2000);
+        }
+    }, 50);
+}
+
+typeStuff();
